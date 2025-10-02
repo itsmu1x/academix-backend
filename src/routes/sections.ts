4a7e936a14846139b734e627273ebded01f3c502
Majd Muhtaseb <itsmu1x+j8kkes58yh@gmail.com>
2025-10-02 17:34:53 +0300

    rest: update schema

--- src/routes/sections.ts
+++ src/routes/sections.ts
@@ -1,11 +1,19 @@
 import { getSections, updateSection } from "src/controllers/sections"
 import { Router } from "express"
 import validate from "src/middleware/validation"
-import { updateSectionSchema } from "src/schemas/sections"
+import {
+	updateSectionParamsSchema,
+	updateSectionSchema,
+} from "src/schemas/sections"
 
 const router = Router()
 
 router.get("/", getSections)
-router.put("/:id", validate(updateSectionSchema, "params"), updateSection)
+router.put(
+	"/:id",
+	validate(updateSectionParamsSchema, "params"),
+	validate(updateSectionSchema),
+	updateSection
+)
 
 export default router
--- src/schemas/sections.ts
+++ src/schemas/sections.ts
@@ -12,6 +12,10 @@ export const updateSectionSchema = z.object({
 	title: z.string(),
 })
 
+export const updateSectionParamsSchema = z.object({
+	id: z.coerce.number().positive(),
+})
+
 export type CreateSectionSchema = z.infer<typeof createSectionSchema>
 export type GetCourseSectionsSchema = z.infer<typeof getCourseSectionsSchema>
 export type UpdateSectionSchema = z.infer<typeof updateSectionSchema>
