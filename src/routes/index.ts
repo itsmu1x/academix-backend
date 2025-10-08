9b15ef4135ec5d78e0a47d842cf694182054e493
Majd Muhtaseb <itsmu1x+j8kkes58yh@gmail.com>
2025-10-08 17:07:00 +0300

    remove docs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import authRoutes from "./auth"
 import categoriesRoutes from "./categories"
 import coursesRoutes from "./courses"
 import sectionsRoutes from "./sections"
-import docsRoutes from "./docs"
 
 const router = express.Router()
 
@@ -11,7 +10,6 @@ router.use("/auth", authRoutes)
 router.use("/categories", categoriesRoutes)
 router.use("/courses", coursesRoutes)
 router.use("/sections", sectionsRoutes)
-router.use("/docs", docsRoutes)
 // TODO: most of the routes are NOT protected, till next push i guess
 
 export default router
